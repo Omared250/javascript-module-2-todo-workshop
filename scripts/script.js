77366634927922ff0f4a566a46a37a69f3778407
Omar <omared250@gmail.com>
2021-07-29 22:33:45 +0200

    exercise 2

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,2 @@
-alert("hello!");
-
-document.querySelector('#new-todo').addEventListener('submit', (e) => {
-    e.preventDefault();
-    const text = e.target.elements.text.value.trim();
-    alert(text);
-})
+const el = document.querySelector(".button");
+console.log(el);
