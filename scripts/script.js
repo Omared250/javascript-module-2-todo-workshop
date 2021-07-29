c5fa6dca451473d6669328dd769d7e00f72b49d7
Omar <omared250@gmail.com>
2021-07-29 23:15:03 +0200

    exercise 4

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,7 @@
-const el = document.querySelector(".button");
-el.style.backgroundColor = "green"
+const el = document.querySelector("#new-todo");
+
+el.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const text = e.target.elements.text.value.trim()
+    console.log(text)
+});
