be9c608aef517cba4784e3301a2dcfa0a4716f31
Omar <omared250@gmail.com>
2021-07-30 23:17:28 +0200

    fixing exercises

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,23 @@
+const el = document.querySelector('#new-todo')
+
+el.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const text = event.target.elements.text.value.trim()
+
+    if (text.length > 0) {
+        createTodo(text)
+        event.target.elements.text.value = ''
+    }
+
+    console.log(todos);
+})
+
+const todos = [];
+
+const createTodo = (text) => {
+    todos.push(text)
+}
+
 const generateTodoDOM = (todo) => {
     const todoEl = document.createElement('label')
     const containerEl = document.createElement('div')
@@ -8,8 +28,17 @@ const generateTodoDOM = (todo) => {
     containerEl.appendChild(todoText)
 
     todoEl.classList.add('list-item')
-    containerEl.classList.add('list-item_container')
+    containerEl.classList.add('list-item__container')
     todoEl.appendChild(containerEl)
 
     return todoEl
+}
+
+const renderTodos = (todos) => {
+    const todoList = document.querySelector('#todos')
+    todoList.innerHTML = ''
+
+    todos.forEach((todo) => {
+        todoList.appendChild(generateTodoDOM(todo))        
+    })
 }
